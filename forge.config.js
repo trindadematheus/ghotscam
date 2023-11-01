fabeeb506d7ba0a5f312d7f8acf2bae45ff77fa9
Matheus Trindade <trindadematheus27@gmail.com>
2023-11-01 16:55:27 -0300

    checkpoint

--- forge.config.js
+++ forge.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   packagerConfig: {
     asar: true,
-    icon: "./icon.png",
+    icon: "/icon",
   },
   rebuildConfig: {},
   makers: [
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, screen } = require("electron");
+const { app, BrowserWindow, screen, Tray, Menu } = require("electron");
 const path = require("path");
 
 if (require("electron-squirrel-startup")) {
@@ -7,17 +7,24 @@ if (require("electron-squirrel-startup")) {
 
 const createWindow = () => {
   const primaryDisplay = screen.getPrimaryDisplay();
-  const { width } = primaryDisplay.workAreaSize;
+  const { width: screenWidth, height: screenHeight } =
+    primaryDisplay.workAreaSize;
+
+  const iconPath = path.join(__dirname, "..", "icon.png");
+
+  const browserWidth = 440;
+  const browserHeight = 260;
 
   const mainWindow = new BrowserWindow({
-    width: 440,
-    height: 260,
+    width: browserWidth,
+    height: browserHeight,
     frame: false,
-    x: width - 450,
+    x: 10,
     y: 10,
     alwaysOnTop: true,
     resizable: false,
-    icon: path.join(__dirname, "..", "icon.png"),
+    focusable: false,
+    icon: iconPath,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
@@ -25,6 +32,123 @@ const createWindow = () => {
 
   mainWindow.setIgnoreMouseEvents(true);
   mainWindow.loadFile(path.join(__dirname, "index.html"));
+
+  const tray = new Tray(iconPath);
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Position",
+      type: "submenu",
+      submenu: [
+        {
+          label: "TOP-LEFT",
+          type: "normal",
+          click: () => mainWindow.setPosition(10, 10),
+        },
+        {
+          label: "TOP-RIGHT",
+          type: "normal",
+          click: () =>
+            mainWindow.setPosition(screenWidth - (browserWidth + 10), 10),
+        },
+        {
+          label: "BOTTOM-LEFT",
+          type: "normal",
+          click: () =>
+            mainWindow.setPosition(10, screenHeight - (browserHeight + 10)),
+        },
+        {
+          label: "BOTTOM-RIGHT",
+          type: "normal",
+          click: () =>
+            mainWindow.setPosition(
+              screenWidth - (browserWidth + 10),
+              screenHeight - (browserHeight + 10)
+            ),
+        },
+      ],
+    },
+    {
+      type: "separator",
+    },
+    // {
+    //   label: "Video Source",
+    //   type: "submenu",
+    //   submenu: [
+    //     {
+    //       label: "top-left",
+    //       type: "normal",
+    //     },
+    //     {
+    //       label: "top-right",
+    //       type: "normal",
+    //     },
+    //     {
+    //       label: "bottom-right",
+    //       type: "normal",
+    //     },
+    //     {
+    //       label: "bottom-right",
+    //       type: "normal",
+    //     },
+    //   ],
+    // },
+    // {
+    //   type: "separator",
+    // },
+    {
+      label: "Scale",
+      type: "submenu",
+      submenu: [
+        {
+          label: "0.5x",
+          type: "normal",
+          click: () => {
+            mainWindow.setBounds({
+              width: browserWidth * 0.5,
+              height: browserHeight * 0.5,
+            });
+            mainWindow.reload();
+          },
+        },
+        {
+          label: "1x",
+          type: "normal",
+          click: () => {
+            mainWindow.setBounds({
+              width: browserWidth,
+              height: browserHeight,
+            });
+            mainWindow.reload();
+          },
+        },
+        {
+          label: "1.5x",
+          type: "normal",
+          click: () => {
+            mainWindow.setBounds({
+              width: browserWidth * 1.5,
+              height: browserHeight * 1.5,
+            });
+            mainWindow.reload();
+          },
+        },
+      ],
+    },
+    {
+      type: "separator",
+    },
+    {
+      type: "normal",
+      label: "Close",
+      role: "quit",
+      enabled: true,
+    },
+  ]);
+
+  tray.setToolTip("Ghost Cam");
+  tray.on("click", () => tray.popUpContextMenu());
+  tray.setContextMenu(contextMenu);
 };
 
 app.on("ready", createWindow);
